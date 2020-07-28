976961590c68dd1398ed8ddb3cdd95719072b573
shaulv <54745332+shaulv@users.noreply.github.com>
2020-07-29 00:31:51 +0300

    Add files via upload

---
+++ HotelEx/Clients/Guest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clients
+{
+    public class Guest
+    {
+        #region Properties
+        int id;
+        string name;
+        #endregion
+
+        #region Constructor
+        public Guest(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+        #endregion
+
+
+        #region Methods
+        public int GetID()
+        {
+            return this.id;
+        }
+        public string GetName()
+        {
+            return this.name;
+        }
+        public override string ToString()
+        {
+            return $"ID: {this.id}\nName: {this.name}";
+        }
+        #endregion
+    }
+}
---
+++ HotelEx/Hotel/FamilyRoom.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel
+{
+    public class FamilyRoom : Room
+    {
+        public FamilyRoom(int roomNumber, float price) : base(roomNumber, price)
+        {
+        }
+
+        protected override void SetPrice()
+        {
+            int basePrice = 100;
+            int extraPrice = 25;
+            int currentMonth = Convert.ToInt32(DateTime.Now.ToString("MM"));
+
+            if (currentMonth == 7 || currentMonth == 8)
+            {
+                this.price = basePrice + extraPrice;
+            }
+            else
+            {
+                this.price = basePrice;
+            }
+        }
+        protected override void SetFeatures()
+        {
+            this.features = "Family Room: TV, minibar...";
+        }
+    }
+}
